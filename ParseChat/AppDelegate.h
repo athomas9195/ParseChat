69b233544cef78b327540e78e5be483bbdfbdc0c
athomas9195 <anna.tina.thomas@gmail.com>
2021-07-06 08:40:11 -0700

    Initial Commit

---
+++ ParseChat/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ParseChat
+//
+//  Created by Anna Thomas on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
